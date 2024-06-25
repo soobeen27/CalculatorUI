f648cc062845d2dffa759164101591d34391d368
soobeen27 <youdew27@gmail.com>
2024-06-25 13:47:40 +0900

    change x

--- CalculatorUI/Model/DataFile.swift
+++ CalculatorUI/Model/DataFile.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Buttons {
     private init() {}
-    static let multiplMark = "✕"
+    static let multiplMark = "×"
     static let divisionMark = "÷"
     static let texts = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", multiplMark, "AC", "0", "=", divisionMark]
     static let orrangeBtns = ["+", "-", divisionMark, multiplMark, "AC", "="]
